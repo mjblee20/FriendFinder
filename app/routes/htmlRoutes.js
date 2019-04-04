ffeba97865bc0037d7823e04b52bd6702dea1873
Monte Cristo <mjblee20@D-10-19-25-45.dhcp4.washington.edu>
2019-04-04 10:56:13 -0700

    initialized files

---
+++ app/routes/htmlRoutes.js
@@ -0,0 +1,11 @@
+module.exports = function (app) {
+    
+    // GET Route to /survey which should display the survey page.
+
+    app.get
+
+    // A default, catch-all route that leads to home.html which displays the home page.
+}
+
+
+
